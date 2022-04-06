f555ef6e6c30b9c3e86d2e27c5dfd5dbf506682e
Callidus2000 <73584079+Callidus2000@users.noreply.github.com>
2022-04-06 16:35:51 +0200

    Dynamic Mapping Support added

---
+++ FortigateManager/functions/Convert-FMIpAddressToMaskLength.ps1
@@ -0,0 +1,22 @@
+﻿function Convert-FMIpAddressToMaskLength{
+    param(
+        [string] $dottedIpAddressString
+        )
+
+    $result = 0;
+    try {
+        # ensure we have a valid IP address
+        [IPAddress] $ip = $dottedIpAddressString;
+        $octets = $ip.IPAddressToString.Split('.');
+        foreach ($octet in $octets) {
+            while (0 -ne $octet) {
+                $octet = ($octet -shl 1) -band [byte]::MaxValue
+                $result++;
+            }
+        }
+    }
+    catch {
+        Write-PSFMessage -Level Warning "No valid IP Mask"
+    }
+    return $result;
+}
