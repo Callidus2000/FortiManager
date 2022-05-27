5c5dea58db1956809a059cf16defc7dd5e66e653
Callidus2000 <73584079+Callidus2000@users.noreply.github.com>
2022-05-27 16:20:41 +0200

    Firewall policies can be added befor/after an existing policyId

--- FortigateManager/functions/Add-FMFirewallPolicy.ps1
+++ FortigateManager/functions/Add-FMFirewallPolicy.ps1
@@ -20,6 +20,12 @@
     .PARAMETER Policy
     The new policy, generated e.g. by using New-FMObjAddress
 
+    .PARAMETER After
+    If used the policy will be added after the policy with the given ID
+
+    .PARAMETER Before
+    If used the policy will be added before the policy with the given ID
+
     .PARAMETER Overwrite
     If used and an policy with the given name already exists the data will be
     overwritten.
@@ -35,14 +41,30 @@
     General notes
     #>
     param (
-        [parameter(Mandatory = $false)]
+        [parameter(Mandatory = $false, ParameterSetName = "default")]
+        [parameter(mandatory = $false, ParameterSetName = "undocumentedAfter")]
+        [parameter(mandatory = $false, ParameterSetName = "undocumentedBefore")]
         $Connection = (Get-FMLastConnection),
         [string]$ADOM,
         [parameter(mandatory = $true, ParameterSetName = "default")]
+        [parameter(mandatory = $false, ParameterSetName = "undocumentedAfter")]
+        [parameter(mandatory = $false, ParameterSetName = "undocumentedBefore")]
         [string]$Package,
+        [parameter(mandatory = $false, ParameterSetName = "undocumentedAfter")]
+        [string]$After,
+        [parameter(mandatory = $false, ParameterSetName = "undocumentedBefore")]
+        [string]$Before,
         [parameter(mandatory = $true, ValueFromPipeline = $true, ParameterSetName = "default")]
+        [parameter(mandatory = $true, ValueFromPipeline = $true, ParameterSetName = "undocumentedAfter")]
+        [parameter(mandatory = $true, ValueFromPipeline = $true, ParameterSetName = "undocumentedBefore")]
         [object[]]$Policy,
+        [parameter(mandatory = $false, ParameterSetName = "default")]
+        [parameter(mandatory = $false, ParameterSetName = "undocumentedAfter")]
+        [parameter(mandatory = $false, ParameterSetName = "undocumentedBefore")]
         [switch]$Overwrite,
+        [parameter(mandatory = $false, ParameterSetName = "default")]
+        [parameter(mandatory = $false, ParameterSetName = "undocumentedAfter")]
+        [parameter(mandatory = $false, ParameterSetName = "undocumentedBefore")]
         [bool]$EnableException = $true
     )
     begin {
@@ -71,6 +93,14 @@
         if ($Overwrite) {
             Write-PSFMessage "Existing data should be overwritten"
             $apiCallParameter.method = "set"
+        }
+        if (-not [string]::IsNullOrEmpty($After)) {
+            Write-PSFMessage "Using undocumented after parameter"
+            $apiCallParameter.Parameter.after = "$after"
+        }
+        if (-not [string]::IsNullOrEmpty($Before)) {
+            Write-PSFMessage "Using undocumented before parameter"
+            $apiCallParameter.Parameter.before = "$Before"
         }
         $result = Invoke-FMAPI @apiCallParameter
         if (-not $EnableException) {
