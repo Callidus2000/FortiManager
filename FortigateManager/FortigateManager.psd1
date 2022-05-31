8150d89e762c40b81fa44cbea93c6d20c6d47e02
Callidus2000 <73584079+Callidus2000@users.noreply.github.com>
2022-05-31 15:17:06 +0200

    -Contributing documentation added, including internal helper -Version increment to v1.2.2

--- FortigateManager/FortigateManager.psd1
+++ FortigateManager/FortigateManager.psd1
@@ -3,7 +3,7 @@
 	RootModule        = 'FortigateManager.psm1'
 
 	# Version number of this module.
-	ModuleVersion     = '1.2.1'
+	ModuleVersion     = '1.2.2'
 
 	# ID used to uniquely identify this module
 	GUID              = '6c74c0d7-80cf-4bef-8fe1-19ac4a89c438'
---
+++ FortigateManager/internal/functions/CodeCreators/Convert-FMApi2Hashtable.ps1
@@ -0,0 +1,106 @@
+﻿function Convert-FMApi2Hashtable {
+    <#
+    .SYNOPSIS
+    Helper function for creating Powershell code out of HashTable Definitions in JSON.
+
+    .DESCRIPTION
+    Helper function for creating Powershell code out of HashTable Definitions in JSON.
+
+    .PARAMETER objectName
+    The new Object Name for the function Name
+
+    .EXAMPLE
+    InModuleScope fortigatemanager {Convert-FMApi2HashTable}
+
+    Takes the JSON Code from the clipboard and replaces it with an auto generated Powershell function.
+
+    .NOTES
+    General notes
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [String]
+        $objectName = ""
+    )
+    Write-Host "ParameterSetName=$ParameterSetName"
+    $json = Get-Clipboard
+
+    $newLineDelim = @"
+,
+
+"@
+    try {
+        $copy2ClipBoardData = @()
+        $copy2ClipBoardData += @"
+function New-FMObj$objectName {
+    [CmdletBinding()]
+    param (
+"@
+        $defParameter = @()
+        $defHashMap = @()
+        $sourceHashTable = $json | ConvertFrom-Json -ErrorAction Stop | convertto-psfhashtable
+        $sourceKeyList = $sourceHashTable.Keys | Sort-Object
+        foreach ($sourceKey in $sourceKeyList) {
+            $parameterName = [regex]::Replace($sourceKey.Trim('_'), '(?i)(?:^|-| )(\p{L})', { $args[0].Groups[1].Value.ToUpper() })
+            $parameterType = $sourceHashTable.$sourceKey.gettype()
+            $parameterValue = $sourceHashTable.$sourceKey
+            Write-Host "`$parameterName=$parameterName; Type=$parameterType;value=$parameterValue"
+            #     $defParameter+=@"
+            #         [parameter(mandatory = `$false, ParameterSetName = "default")]
+            #         [$parameterType]`$$parameterName
+            # "@
+            # Hashtable Definition ergänzen
+            switch ($parameterType) {
+                "long" {
+                    $defParameter += @"
+        [parameter(mandatory = `$false, ParameterSetName = "default")]
+        [$parameterType]`$$parameterName=-1
+"@
+                }
+                Default {
+                    switch -regex ($parameterValue) {
+                        "disable|disable" {
+                            $defParameter += @"
+            [parameter(mandatory = `$false, ParameterSetName = "default")]
+            [ValidateSet("disable", "enable")]
+            [$parameterType]`$$parameterName
+"@
+                        }
+                        Default {
+                            $defParameter += @"
+            [parameter(mandatory = `$false, ParameterSetName = "default")]
+            [$parameterType]`$$parameterName
+"@
+                        }
+                    }
+                }
+            }
+            # Hashtable Definition ergänzen
+            switch ($parameterType) {
+                "System.Object[]" { $defHashMap += "'$sourceKey'=@(`$$parameterName)" }
+                "string" { $defHashMap += "'$sourceKey'=`"`$$parameterName`"" }
+                "long" { $defHashMap += "'$sourceKey'=`$$parameterName" }
+                Default { Write-PSFMessage -Level Warning "Unknown ParamaterType $parameterType" }
+            }
+        }
+        $defParameter += @"
+        [ValidateSet("Keep", "RemoveAttribute", "ClearContent")]
+        [parameter(mandatory = `$false, ValueFromPipeline = `$false, ParameterSetName = "default")]
+        `$NullHandler = "RemoveAttribute"
+"@
+        $copy2ClipBoardData += ($defParameter | Join-String $newLineDelim)
+        $copy2ClipBoardData += ")"
+        $copy2ClipBoardData += "`$data=@{"
+        $copy2ClipBoardData += ($defHashMap | Out-String)
+        $copy2ClipBoardData += "}"
+        $copy2ClipBoardData += "return `$data | Remove-FMNullValuesFromHashtable -NullHandler `$NullHandler"
+        $copy2ClipBoardData += "}"
+        write-host ($copy2ClipBoardData  | out-string)
+        $copy2ClipBoardData  | out-string | Set-Clipboard
+        # write-host ($defParameter | Join-String $newLineDelim | out-string)
+    }
+    catch {
+        Write-Host "Clipboard did not contain a JSON String, $_"
+    }
+}
