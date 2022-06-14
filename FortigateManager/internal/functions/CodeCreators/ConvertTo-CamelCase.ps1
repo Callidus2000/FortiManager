6e3749aeb15b1241c6508c69b9794bd229238137
Callidus2000 <73584079+Callidus2000@users.noreply.github.com>
2022-06-14 12:55:28 +0200

    Additional (yet useless) CodeCreator function 'Convert-FMApiGet2FunctionStub' added

---
+++ FortigateManager/internal/functions/CodeCreators/ConvertTo-CamelCase.ps1
@@ -0,0 +1,8 @@
+﻿function ConvertTo-CamelCase {
+    [CmdletBinding()]
+    param (
+        [Parameter(Position = 0)]
+        [string]$Text
+    )
+    return [regex]::Replace($Text.Trim('_').Trim(' '), '(?i)(?:^|-| )(\p{L})', { $args[0].Groups[1].Value.ToUpper() })
+}
