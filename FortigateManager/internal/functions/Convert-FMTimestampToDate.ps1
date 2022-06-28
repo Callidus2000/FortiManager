5f0a717116b917d6f516eed4768cba1ba8fef930
Callidus2000 <73584079+Callidus2000@users.noreply.github.com>
2022-06-28 11:11:29 +0200

    -Hitcount data will be used from existing results -Force param added for refreshing hitcounts -Version increment to 1.3.4

---
+++ FortigateManager/internal/functions/Convert-FMTimestampToDate.ps1
@@ -0,0 +1,30 @@
+﻿function Convert-FMTimestampToDate {
+    <#
+    .SYNOPSIS
+    Helper function to convert Unix timestamps to DateTime.
+
+    .DESCRIPTION
+    Helper function to convert Unix timestamps to DateTime.
+
+    .PARAMETER TimeStamp
+    The timestamp to be converted
+
+    .EXAMPLE
+    Convert-FMTimestampToDate -TimeStamp 1655983275
+
+    Returns 'Thursday, 23 June 2022 11:21:15'
+
+    .NOTES
+    General notes
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true,
+                   Position=0,
+                   ValueFromPipeline=$true)]
+        $TimeStamp
+    )
+    process{
+        return (Get-Date 01.01.1970) + ([System.TimeSpan]::fromseconds($TimeStamp))
+    }
+}
