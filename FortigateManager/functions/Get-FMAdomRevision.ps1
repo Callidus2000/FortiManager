ff7bec8347bd994c055d9d54b268123dcbbd887b
Callidus2000 <73584079+Callidus2000@users.noreply.github.com>
2022-11-18 14:22:35 +0100

    ADOM revisions can be created/queried/removed

---
+++ FortigateManager/functions/Get-FMAdomRevision.ps1
@@ -0,0 +1,109 @@
+﻿function Get-FMAdomRevision {
+    <#
+    .SYNOPSIS
+    Querys existing ADOM Revisions.
+
+    .DESCRIPTION
+    Querys existing ADOM Revisions.
+
+    .PARAMETER Connection
+    The API connection object.
+
+    .PARAMETER ADOM
+    The (non-default) ADOM for the requests.
+
+    .PARAMETER EnableException
+    If set to True, errors will throw an exception
+
+    .PARAMETER Sortings
+    Specify the sorting of the returned result.
+
+    .PARAMETER Loadsub
+    Enable or disable the return of any sub-objects. If not specified, the default is to return all sub-objects.
+
+    .PARAMETER Option
+    Set fetch option for the request. If no option is specified, by default the attributes of the objects will be returned.
+    count - Return the number of matching entries instead of the actual entry data.
+    scope member - Return a list of scope members along with other attributes.
+    datasrc - Return all objects that can be referenced by an attribute. Require attr parameter.
+    get reserved - Also return reserved objects in the result.
+    syntax - Return the attribute syntax of a table or an object, instead of the actual entry data. All filter parameters will be ignored.
+
+    .PARAMETER ExpandMembers
+    Fetch all or selected attributes of object members.
+    string($expand member object)
+
+    .PARAMETER Filter
+    Filter the result according to a set of criteria. For detailed help
+    see about_FortigateManagerFilter
+
+    .PARAMETER Range
+    Limit the number of output. For a range of [a, n], the output will contain n elements, starting from the ath matching result.
+
+    .PARAMETER Fields
+    Limit the output by returning only the attributes specified in the string array. If none specified, all attributes will be returned.
+
+    .PARAMETER LoggingLevel
+    On which level should die diagnostic Messages be logged?
+
+    .PARAMETER NullHandler
+    Parameter description
+
+    .EXAMPLE
+    Get-FMAdomRevision
+
+    Lists the existing Adom Revisions
+
+    .NOTES
+    General notes
+    #>
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory = $false)]
+        $Connection = (Get-FMLastConnection),
+        [string]$ADOM,
+        [bool]$EnableException = $true,
+
+        [parameter(mandatory = $false, ParameterSetName = "default")]
+        [System.Object[]]$Sortings,
+        [parameter(mandatory = $false, ParameterSetName = "default")]
+        [ValidateSet("count", "object member", "syntax")]
+        [string]$Option,
+        [parameter(mandatory = $false, ParameterSetName = "default")]
+        [string]$ExpandMembers,
+        [parameter(mandatory = $false, ParameterSetName = "default")]
+        [string[]]$Filter,
+        [parameter(mandatory = $false, ParameterSetName = "default")]
+        [System.Object[]]$Range,
+        [parameter(mandatory = $false, ParameterSetName = "default")]
+        [ValidateSet("created_by", "created_time", "desc", "locked", "name", "version")]
+        [System.Object[]]$Fields,
+        [string]$LoggingLevel,
+        [ValidateSet("Keep", "RemoveAttribute", "ClearContent")]
+        [parameter(mandatory = $false, ParameterSetName = "default")]
+        $NullHandler = "RemoveAttribute"
+    )
+    $Parameter = @{
+        'sortings' = @($Sortings)
+        'option'   = "$Option"
+        'filter'   = ($Filter | ConvertTo-FMFilterArray)
+        'range'    = @($Range)
+        'fields'   = @($Fields)
+        'expand members'="$ExpandMembers"
+    } | Remove-FMNullValuesFromHashtable -NullHandler $NullHandler
+    $explicitADOM = Resolve-FMAdom -Connection $Connection -Adom $ADOM -EnableException $EnableException
+    $apiCallParameter = @{
+        EnableException     = $EnableException
+        Connection          = $Connection
+        LoggingAction       = "Get-FMAdomRevision"
+        LoggingActionValues = $explicitADOM
+        method              = "get"
+        Parameter           = $Parameter
+        Path                = "/dvmdb/adom/$explicitADOM/revision"
+    }
+    if (-not [string]::IsNullOrEmpty($LoggingLevel)) { $apiCallParameter.LoggingLevel = $LoggingLevel }
+
+    $result = Invoke-FMAPI @apiCallParameter
+    Write-PSFMessage "Result-Status: $($result.result.status)"
+    return $result.result.data
+}
---
+++ FortigateManager/functions/Remove-FMAdomRevision.ps1
@@ -0,0 +1,83 @@
+﻿function Remove-FMAdomRevision {
+    <#
+    .SYNOPSIS
+    Removes an existing ADOM revision.
+
+    .DESCRIPTION
+    Removes an existing ADOM revision.
+
+    .PARAMETER Connection
+    The API connection object.
+
+    .PARAMETER ADOM
+    The (non-default) ADOM for the requests.
+
+    .PARAMETER Version
+    Version number of the revision to be removed.
+
+  	.PARAMETER EnableException
+	Should Exceptions been thrown?
+
+    .EXAMPLE
+    Remove-FMAdomRevision -Version 3
+
+    Removes the revision with the ID 3
+    .NOTES
+    General notes
+    #>
+    [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'Medium')]
+    param (
+        [parameter(Mandatory = $false)]
+        $Connection = (Get-FMLastConnection),
+        [string]$ADOM,
+        [parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [string[]]$Version,
+        [bool]$EnableException = $true
+    )
+    begin {
+        $explicitADOM = Resolve-FMAdom -Connection $Connection -Adom $ADOM
+        Write-PSFMessage "`$explicitADOM=$explicitADOM"
+        $apiCallParameter = @{
+            EnableException     = $EnableException
+            RevisionNote        = $RevisionNote
+            Connection          = $Connection
+            LoggingActionValues = $explicitADOM
+            method              = "delete"
+            parameter           = @()
+        }
+        $revisionList = @()
+    }
+    process {
+        # foreach($id in $version){
+        #     Write-PSFMessage "$id = $($id.GetType())"
+        #     $revisionList+=$id
+        # }
+        $revisionList += $Version
+    }
+    end {
+        # Removing potential Null values
+        $revisionList = $revisionList | Where-Object { $_ }
+        Write-PSFMessage "Removing revisions $($revisionList -join ',')"
+        # Different Logging if multiple or single address should be deleted
+        if ($revisionList.count -gt 1) {
+            $apiCallParameter.LoggingAction = "Remove-FMAdomRevision-Multiple"
+            $apiCallParameter.LoggingActionValues = $revisionList.count
+        }
+        else {
+            $apiCallParameter.LoggingAction = "Remove-FMAdomRevision"
+            $apiCallParameter.LoggingActionValues = $revisionList
+        }
+        foreach ($id in $revisionList) {
+            $dataSet = @{
+                url = "/dvmdb/adom/$explicitADOM/revision/$id"
+            }
+            $apiCallParameter.parameter += $dataSet
+        }
+        $result = Invoke-FMAPI @apiCallParameter
+        # If EnableException an exception would have be thrown, otherwise the function returns true for success or false for failure
+        if (-not $EnableException) {
+            return ($null -ne $result)
+        }
+    }
+
+}
