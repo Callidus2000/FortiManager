795b6f960ce9a730370530f5d215565e405e4fca
Callidus2000 <73584079+Callidus2000@users.noreply.github.com>
2022-11-27 14:18:41 +0100

    Added stub for Convert-FMZone2VLAN

---
+++ FortigateManager/functions/Convert-FMZone2VLAN.ps1
@@ -0,0 +1,41 @@
+﻿function Convert-FMZone2VLAN {
+    <#
+    .SYNOPSIS
+    Short description
+
+    .DESCRIPTION
+    Long description
+
+    .PARAMETER Connection
+    The API connection object.
+
+    .PARAMETER ADOM
+    The (non-default) ADOM for the requests.
+
+    .PARAMETER LoggingLevel
+    On which level should die diagnostic Messages be logged?
+
+    .PARAMETER EnableException
+    If set to True, errors will throw an exception
+
+    .EXAMPLE
+    An example
+
+    .NOTES
+    General notes
+    #>
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory = $false)]
+        $Connection = (Get-FMLastConnection),
+        [string]$ADOM,
+        [parameter(Mandatory = $true)]
+        [string[]]$Zone,
+        [ValidateSet('simpleIpList')]
+        $ReturnType,
+        [string]$LoggingLevel,
+        [bool]$EnableException = $true
+    )
+    Write-PSFMessage "Determine VLANs from Zones: $($Zone -join ',')"
+
+}
